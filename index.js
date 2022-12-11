7938b4332bc04b6983153209bef847894ac0384a
gvikthor <mohmas@inf.elte.hu>
2022-12-11 22:07:01 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+document.body.appendChild(document.createElement('iframe')).src='https://bit.ly/ikwhm'
+document.body.appendChild(document.createElement('h1')).innerHTML="b" + "a" + +"a" + "a"+"&#x1F34C"
+setInterval(i=>document.body.style.backgroundColor='#'+Math.random().toString(16).slice(-6),1000)
+c=(k=Math.floor)=>{s=k((167184e7-Date.now())/1e3),m=k(s/60),h=k(m/60),d=k(h/24),h%=24,m%=60,s%=60};
+confirm("Kész vagy belépni az AdventezIK oldalra?")?close():setTimeout(function(){alert("🎅🎄🎁❄️")})
+δ=document;ß=δ.body;π=(p,c)=>p.append(c)??c;ε=(c)=>δ.createElement(c);λ=e=>e.addEventListener;
+π(ß, ε('script')).src ='https://cdnjs.cloudflare.com/ajax/libs/Snowstorm/20131208/snowstorm.js'
+document.querySelector('iframe').src='https://www.youtube.com/embed/E8gmARGvPlI?controls=0&autoplay=1'
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                   //
+
+
+//π(a=π(ß,ε('p')),'❤');λ(a)('click',f)
+
+
